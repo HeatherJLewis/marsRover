eef3496162b3066b1d1b8136c1cba7f5c99ca6af
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-11 12:04:16 +0000

    marsRover4 Adds a new /apod endpoint

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const router = require('./routes')
 
 app.use(express.static('frontend'));
+
+app.use(router);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
