10803d9c8e1d269b3926524b03e3b0fdd04c3e41
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-11 15:51:59 +0000

    marsRover7 fixes small error

--- routes.js
+++ routes.js
@@ -6,6 +6,6 @@ router.use('/login', express.static('frontend/login.html'));
 
 router.get('/apod', (request, response) => {
     response.send("Astronomy Photo Of the Day!");
-})
+});
 
 module.exports = router;
