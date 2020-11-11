a35cbc2d5d69f87f217093c2711907fe7c2a2ea8
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-11 11:11:56 +0000

    marsRover2 Adds semicolons to index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.use(express.static('frontend'))
+app.use(express.static('frontend'));
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
