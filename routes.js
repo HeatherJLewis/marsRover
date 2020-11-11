6be2e4e99311e8507a78077e5a28e30fed79ffbc
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-11 15:42:31 +0000

    marsRover7 Adds a login page

--- routes.js
+++ routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+router.use('/login', express.static('frontend/login.html'))
+
 router.get('/apod', (request, response) => {
     response.send("Astronomy Photo Of the Day!")
-});
+})
 
 module.exports = router;
