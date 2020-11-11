a9507537d8aa574a734e5350c776a9b4c9a30410
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-11 12:04:54 +0000

    marsRover4 Adds missing semicolons

--- routes.js
+++ routes.js
@@ -4,6 +4,6 @@ const router = express.Router();
 
 router.get('/apod', (request, response) => {
     response.send("Astronomy Photo Of the Day!")
-})
+});
 
-module.exports = router
+module.exports = router;
