8a15bb0abe2cf23e3c88e09b75684c07595fee57
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-12 15:43:15 +0000

    marsRover11 Adds the time zone difference to the homepage

--- frontend/index.js
+++ frontend/index.js
@@ -1,5 +1,17 @@
+const todaysDate = new Date()
+
 const dateHeader = document.getElementById('dateHeading');
-
-const dateToday = document.createTextNode(String(new Date()));
-
+const dateToday = document.createTextNode(String(todaysDate.toLocaleString()));
 dateHeader.append(dateToday);
+
+const usersHoursFromGMT = (todaysDate.getTimezoneOffset())/60;
+const usersHoursFromKennedy = usersHoursFromGMT + 5;
+
+const kennedyTimeHeader = document.getElementById('kennedyTime');
+const kennedyTimeZoneOffset = document.createTextNode(usersHoursFromKennedy);
+kennedyTime.append(kennedyTimeZoneOffset);
+
+const hoursAhead = document.createTextNode(' hours ahead of the Kennedy Space Centre');
+const hoursBehind = document.createTextNode(' hours behind the Kennedy Space Centre');
+
+usersHoursFromKennedy > 0 ? kennedyTimeHeader.append(hoursAhead) : kennedyTimeHeader.append(hoursBehind);
