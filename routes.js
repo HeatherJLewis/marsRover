1894207624dfeb0d163f5469855cba670862b7fe
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-13 14:29:56 +0000

    marsRover18 creates a jwt with the username and password

--- routes.js
+++ routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
+
+const privateKey = 'WeLoveAstronomy!';
 
 router.use('/login', express.static('frontend/login.html'));
 
 router.post('/authenticate', (request, response) => {
-	response.send(request.body);
+	const { username , password } = request.body;
+	const token = jwt.sign({username, password}, privateKey );
+
+	response.header('Authorization', 'bearer ' +token);
+	response.send('We have made a JWT for you!');
 });
 
 module.exports = router;
