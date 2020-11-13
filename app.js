f16f09444875cbd6fbdc9b75dcec734910e82365
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-13 13:52:26 +0000

    marsRover17 Sends form data from login page to endpoint

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ const port = 3000;
 const router = require('./routes');
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(express.urlencoded({
+	extended: true
+}));
+
 app.use(express.static('frontend'));
 app.use(router);
 
