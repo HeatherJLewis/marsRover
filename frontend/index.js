3fd09da7b19e0b475651b61154e67762b2c18cd9
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-13 11:27:11 +0000

    marsRover12 Fixes eslint run cmd and linting errors

--- frontend/index.js
+++ frontend/index.js
@@ -1,30 +1,28 @@
 const addTodaysDate = () => {
-    const dateToday = document.createTextNode(String((new Date()).toLocaleString()));
-    dateHeader = document.getElementById('dateHeading')
-                         .append(dateToday);
+	const dateToday = document.createTextNode(String((new Date()).toLocaleString()));
+
+	document.getElementById('dateHeading')
+		.append(dateToday);
 };
 
 const addTimeZoneDifference = () => {
-    const usersHoursFromGMT = ((new Date()).getTimezoneOffset())/60;
-    const usersHoursFromKennedy = usersHoursFromGMT + 5;
+	const usersHoursFromGMT = ((new Date()).getTimezoneOffset())/60;
+	const usersHoursFromKennedy = usersHoursFromGMT + 5;
 
-    const kennedyTimeZoneOffset = document.createTextNode(usersHoursFromKennedy);
-    kennedyTime.append(kennedyTimeZoneOffset);
+	const aheadBehindText = usersHoursFromKennedy > 0 ? 'ahead of' : 'behind';
+	const hoursAheadBehind = document.createTextNode(`${usersHoursFromKennedy} hours ${aheadBehindText} the Kennedy Space Centre`);
 
-    const aheadBehindText = usersHoursFromKennedy > 0 ? "ahead of" : "behind"
-
-    const hoursAheadBehind = document.createTextNode(` hours ${aheadBehindText} the Kennedy Space Centre`);
-
-    kennedyTimeHeader = document.getElementById('kennedyTime')
-                                .append(hoursAheadBehind);
+	document.getElementById('kennedyTime')
+            .append(hoursAheadBehind);
 };
 
 const addCurrentTimeAtKennedySpaceCentre = () => {
-    const kennedyTime = (new Date()).toLocaleTimeString(undefined, {
-        timeZone: "America/New_York"
-    })
-    return currentKennedyTime = document.getElementById('kennedyTimeNow')
-                                        .append(kennedyTime);
+	const kennedyTime = (new Date()).toLocaleTimeString(undefined, {
+		timeZone: 'America/New_York'
+	});
+
+	document.getElementById('kennedyTimeNow')
+		.append(kennedyTime);
 };
 
 addTodaysDate();
