510f8cb3bbe721accc830ae7616a8e0e44bcbe77
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-16 11:59:40 +0000

    marsRover23 Installs lint-staged and prettier

--- routes.js
+++ routes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const { privateKey } = require('./config/apiCredentials');
+const { PRIVATE_KEY } = require('./config/apiCredentials');
 
 const router = express.Router();
 
 router.use('/login', express.static('frontend/login.html'));
 
 router.post('/authenticate', (request, response) => {
-	const { username , password } = request.body;
-	const token = jwt.sign({username, password}, privateKey );
+	const { username, password } = request.body;
+	const token = jwt.sign({ username, password }, PRIVATE_KEY);
 
 	response.cookie('access_token', token, { httpOnly: true });
 	response.send('We have made a JWT for you!');
