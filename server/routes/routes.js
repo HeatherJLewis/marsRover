a9c8c4f3df3f709650b03da1b494830336f8e6a6
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-18 08:42:13 +0000

    marsRover13 Adds apod endpoint for retrieval of image and explanation

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const { checkForUser } = require('../middleware/checkForUser');
 const { setJwtOnAccessToken } = require('../middleware/setJwt');
+const { getApodImageAndExplanation } = require('../middleware/getApod');
 
 const router = express.Router();
 
 router.use('/login', express.static('app/login'));
+router.use('/rover', express.static('app/marsRoverPath'));
+
+router.get('/apod', getApodImageAndExplanation);
 
 router.post('/authenticate', checkForUser, setJwtOnAccessToken);
-
-router.use('/rover', express.static('app/marsRoverPath'));
 
 module.exports = router;
