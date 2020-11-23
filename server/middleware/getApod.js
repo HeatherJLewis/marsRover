5a5b291882ac5089a23271099f068d0dde70baad
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-23 16:49:26 +0000

    marsRover13 Separates middleware and business logic

--- server/middleware/getApod.js
+++ server/middleware/getApod.js
@@ -1,27 +1,8 @@
-const axios = require('axios');
-const { NASA_API_KEY } = require('../../config/apiCredentials');
-
-const queryParameters = {
-	params: {
-		api_key: NASA_API_KEY,
-	},
-};
-
-const getApod = async () => {
-	try {
-		const apodData = await axios.get(
-			'https://api.nasa.gov/planetary/apod',
-			queryParameters
-		);
-		return apodData;
-	} catch (error) {
-		return Promise.reject(new Error(`${error.name}: ${error.message}`));
-	}
-};
+const { getPhotoOfTheDay } = require('../services/getPhotoOfTheDay');
 
 const getApodImageAndExplanation = async (request, response) => {
 	try {
-		const body = await getApod();
+		const body = await getPhotoOfTheDay();
 		const { url, explanation } = body.data;
 		response.send({ url, explanation });
 	} catch (error) {
---
+++ server/services/getPhotoOfTheDay.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+const { NASA_API_KEY } = require('../../config/apiCredentials');
+
+const queryParameters = {
+	params: {
+		api_key: NASA_API_KEY,
+	},
+};
+
+const getPhotoOfTheDay = async () => {
+	try {
+		const apodData = await axios.get(
+			'https://api.nasa.gov/planetary/apod',
+			queryParameters
+		);
+		return apodData;
+	} catch (error) {
+		return Promise.reject(new Error(`${error.name}: ${error.message}`));
+	}
+};
+
+module.exports = {
+	getPhotoOfTheDay,
+};
