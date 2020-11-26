548d9710dbcd96f43a80da492d90c72b080d07eb
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-26 16:06:36 +0000

    marsRover15: Retrieves the username and serves as JSON on the /getUsername endpoint

--- server/middleware/getUsername.js
+++ server/middleware/getUsername.js
@@ -1,9 +1,15 @@
 const jwtDecode = require('jwt-decode');
 
 const getUsername = (request, response) => {
+<<<<<<< HEAD
 	const accessToken = request.cookies.access_token;
 	const { username } = jwtDecode(accessToken);
 	response.json({ username });
+=======
+	const accessToken = request.headers.cookie.split('=')[1];
+	const { username } = jwtDecode(accessToken);
+	response.send(username);
+>>>>>>> marsRover15: Retrieves the username and serves as JSON on the /getUsername endpoint
 };
 
 module.exports = {
