7114b58d51a0de36b2b8dfac6e347e138c225066
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-27 11:29:15 +0000

    marsRover15: Resolves refactor requests from PR

--- server/middleware/getUsername.js
+++ server/middleware/getUsername.js
@@ -1,7 +1,7 @@
 const jwtDecode = require('jwt-decode');
 
 const getUsername = (request, response) => {
-	const accessToken = request.headers.cookie.split('=')[1];
+	const accessToken = request.cookies.access_token;
 	const { username } = jwtDecode(accessToken);
 	response.json({ username });
 };
