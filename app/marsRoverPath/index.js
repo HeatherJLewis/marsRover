9de87e464eba43a68f64cf32a291ceb24c12d586
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-28 12:07:20 +0000

    marsRover56: Stays on approximate rover path during drag and drop

--- app/marsRoverPath/index.js
+++ app/marsRoverPath/index.js
@@ -13,7 +13,7 @@ const positionRoverAtStartOfPath = () => {
 		roverMap.offsetHeight * scalingFactorFromTop +
     roverMap.getBoundingClientRect().top
 	}px`;
-
+	console.log('x', spriteImage.style.left, 'y', spriteImage.style.top);
 	window.addEventListener('resize', () => {
 		spriteImage.style.left = `${
 			roverMap.offsetWidth * scalingFactorFromLeft +
@@ -44,7 +44,7 @@ const dragAndDropRover = () => {
 
 	let active = false;
 
-	const roverSprite = document.getElementById('#rover-sprite');
+	const roverSprite = document.getElementById('rover-sprite');
 
 	const dragStart = (event) => {
 		initialMousePosition.x = event.clientX - offset.x;
@@ -58,6 +58,13 @@ const dragAndDropRover = () => {
 		initialMousePosition.x = currentMousePosition.x;
 		initialMousePosition.y = currentMousePosition.y;
 
+		console.log(
+			'DRAG END',
+			'x',
+			initialMousePosition.x,
+			'y',
+			initialMousePosition.y
+		);
 		active = false;
 	};
 
@@ -65,11 +72,39 @@ const dragAndDropRover = () => {
 		element.style.transform = 'translate(' + xPos + 'px, ' + yPos + 'px)';
 	};
 
+	const calculateXcoordinateUpper = (yCoordinate) => {
+		const gradient = -545 / 500;
+		const constant = 700 - 600 * gradient;
+		return (yCoordinate - constant) / gradient;
+	};
+
+	const calculateXcoordinateMiddle = (yCoordinate) => {
+		const gradient = 155 / 80;
+		const constant = 480 - 600 * gradient;
+		return (yCoordinate - constant) / gradient;
+	};
+
+	const calculateXcoordinateLower = (yCoordinate) => {
+		const gradient = 100 / 120;
+		const constant = 705 - 680 * gradient;
+		return (yCoordinate - constant) / gradient;
+	};
+
 	const drag = (event) => {
 		if (active) {
 			event.preventDefault();
-			currentMousePosition.x = event.clientX - initialMousePosition.x;
+			if (event.clientY - initialMousePosition.y < 545) {
+				currentMousePosition.x =
+          calculateXcoordinateUpper(event.clientY) - initialMousePosition.x;
+			} else if (event.clientY - initialMousePosition.y < 700) {
+				currentMousePosition.x =
+          calculateXcoordinateMiddle(event.clientY) - initialMousePosition.x;
+			} else {
+				currentMousePosition.x =
+          calculateXcoordinateLower(event.clientY) - initialMousePosition.x;
+			}
 			currentMousePosition.y = event.clientY - initialMousePosition.y;
+			// currentMousePosition.x = event.clientX - initialMousePosition.x;
 
 			offset.x = currentMousePosition.x;
 			offset.y = currentMousePosition.y;
@@ -77,6 +112,11 @@ const dragAndDropRover = () => {
 			setTranslate(currentMousePosition.x, currentMousePosition.y, roverSprite);
 		}
 	};
+
+	const mapImage = document.getElementById('rover-path-image');
+
+	mapImage.addEventListener('mousemove', drag, false);
+	mapImage.addEventListener('mouseup', dragEnd, false);
 
 	roverSprite.addEventListener('mousedown', dragStart, false);
 	roverSprite.addEventListener('mouseup', dragEnd, false);
