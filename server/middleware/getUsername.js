16a169163e9b56929634b0979fcf2c3055a9adbe
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-30 15:40:56 +0000

    marsRover60 getUsername checks if there is a username before calling jwtDecode

--- server/middleware/getUsername.js
+++ server/middleware/getUsername.js
@@ -3,11 +3,14 @@ const jwtDecode = require('jwt-decode');
 const getUsername = (request, response) => {
 	const accessToken = request.cookies.access_token;
 	try {
-		const { username } = jwtDecode(accessToken);
-		response.json({ username });
+		if (accessToken) {
+			const { username } = jwtDecode(accessToken);
+			response.json({ username });
+		} else {
+			response.json({});
+		}
 	} catch (error) {
 		console.log(error);
-		response.json({});
 	}
 };
 
