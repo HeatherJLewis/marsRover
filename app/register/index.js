64decf01a21e954862adb2284968dd838f880b83
Heather Lewis <heather.lewis@bbc.co.uk>
2020-11-30 17:00:09 +0000

    marsRover65 Adds basic password validation

--- app/register/index.js
+++ app/register/index.js
@@ -1,9 +1,71 @@
 const validatePassword = () => {
 	const passwordElement = document.getElementById('password');
 
-	passwordElement.addEventListener('change', (event) => {
-		console.log(event.target.value);
-	});
+	const checkPasswordLength = (event) => {
+		if (event.target.value.length < 8) {
+			const passwordWarning = document.createElement('p');
+			passwordWarning.id = 'password-warning-length';
+			passwordWarning.textContent =
+        'Your password is too short. It should have more than 8 characters.';
+			document.getElementById('password-input').appendChild(passwordWarning);
+		}
+	};
+
+	const checkPasswordContainsAtLeastOneNumber = (event) => {
+		const regEx = /(\d)/;
+		if (!regEx.test(event.target.value)) {
+			const passwordWarning = document.createElement('p');
+			passwordWarning.textContent =
+        'Your password needs to contain at least one number.';
+			passwordWarning.id = 'password-warning-number';
+			document.getElementById('password-input').appendChild(passwordWarning);
+		}
+	};
+
+	const checkPasswordContainsAtLeastOneLowercaseCharacter = (event) => {
+		const regEx = /([a-z])/;
+		if (!regEx.test(event.target.value)) {
+			const passwordWarning = document.createElement('p');
+			passwordWarning.textContent =
+        'Your password needs to contain at least one lowercase character.';
+			passwordWarning.id = 'password-warning-lowercase';
+			document.getElementById('password-input').appendChild(passwordWarning);
+		}
+	};
+
+	const checkPasswordContainsAtLeastOneUppercaseCharacter = (event) => {
+		const regEx = /([A-Z])/;
+		if (!regEx.test(event.target.value)) {
+			const passwordWarning = document.createElement('p');
+			passwordWarning.textContent =
+        'Your password needs to contain at least one uppercase character.';
+			passwordWarning.id = 'password-warning-uppercase';
+			document.getElementById('password-input').appendChild(passwordWarning);
+		}
+	};
+
+	const clearInputOnFocus = (event) => {
+		event.target.value = '';
+		document.getElementById('password-warning-length').remove();
+		document.getElementById('password-warning-number').remove();
+		document.getElementById('password-warning-lowercase').remove();
+		document.getElementById('password-warning-uppercase').remove();
+	};
+
+	passwordElement.addEventListener(
+		'change',
+		checkPasswordContainsAtLeastOneUppercaseCharacter
+	);
+	passwordElement.addEventListener(
+		'change',
+		checkPasswordContainsAtLeastOneLowercaseCharacter
+	);
+	passwordElement.addEventListener(
+		'change',
+		checkPasswordContainsAtLeastOneNumber
+	);
+	passwordElement.addEventListener('change', checkPasswordLength);
+	passwordElement.addEventListener('focus', clearInputOnFocus);
 };
 
 validatePassword();
