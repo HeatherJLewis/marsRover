39f6c720613f497d50fca82f7223f4f115d6be96
Heather Lewis <heather.lewis@bbc.co.uk>
2020-12-10 13:56:45 +0000

    marsRover75: Adds insertJtiIntoDb into route

--- server/middleware/insertJtiIntoDB.js
+++ server/middleware/insertJtiIntoDB.js
@@ -8,12 +8,12 @@ const insertJtiIntoDB = (request, response, next) => {
 	const TEN_MINS_IN_SECONDS = 10 * 60;
 	const expiryTime = Math.floor(Date.now() / 1000) + TEN_MINS_IN_SECONDS;
 
-	request.body.exp = expiryTime;
-	request.body.jti = jti;
-
 	JwtIds.create({ jti, exp: expiryTime, userId }).catch((error) => {
 		logger.warn(`${error.title}: ${error.message}`);
 	});
+
+	request.body.exp = expiryTime;
+	request.body.jti = jti;
 
 	next();
 };
