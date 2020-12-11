9a283ad070dc75c1961bdb8aa029afa5bc80f4e2
Heather Lewis <heather.lewis@bbc.co.uk>
2020-12-11 13:28:43 +0000

    marsRover76: Renames expiry time constant

--- server/middleware/insertJtiIntoDB.js
+++ server/middleware/insertJtiIntoDB.js
@@ -1,13 +1,15 @@
 const { JwtIds } = require('../../database/models');
 const { v4: uuidv4 } = require('uuid');
 const logger = require('../../config/logger');
-const { JWT_EXPIRY_TIME_IN_SECONDS } = require('../../config/apiCredentials');
+const {
+	JWT_EXPIRATION_TIME_IN_SECONDS,
+} = require('../../config/apiCredentials');
 
 const insertJtiIntoDB = (request, response, next) => {
 	const { userId } = request.body;
 	const jti = uuidv4();
 	const expiryTimeInSeconds =
-    Math.floor(Date.now() / 1000) + JWT_EXPIRY_TIME_IN_SECONDS;
+    Math.floor(Date.now() / 1000) + JWT_EXPIRATION_TIME_IN_SECONDS;
 
 	JwtIds.create({ jti, exp: expiryTimeInSeconds, userId }).catch((error) => {
 		logger.warn(`${error.title}: ${error.message}`);
