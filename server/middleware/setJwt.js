bd2b01c2c7ec05007b70698c77bbc35e0cd57999
Heather Lewis <heather.lewis@bbc.co.uk>
2020-12-11 09:51:40 +0000

    marsRover75c: Removes password and username from the JWT

--- server/middleware/setJwt.js
+++ server/middleware/setJwt.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 const { PRIVATE_KEY } = require('../../config/apiCredentials');
 
 const setJwtOnAccessToken = (request, response) => {
-	const { username, password, exp, jti } = request.body;
-	const token = jwt.sign({ username, password, exp, jti }, PRIVATE_KEY);
+	const { exp, jti } = request.body;
+	const token = jwt.sign({ exp, jti }, PRIVATE_KEY);
 
 	response.cookie('access_token', token, { httpOnly: true });
 	response.redirect('/');
